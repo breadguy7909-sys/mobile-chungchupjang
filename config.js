6794a4dc13cb51c003705277d557e1ece3519f74
breadguy7909-sys <breadguy7909@snu.ac.kr>
2026-01-03 01:42:35 +0900

    config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ export const KAKAOTALK_SHARE_IMAGE =
 export const WEDDING_DATE = "1970년 01월 01일, 목요일 오전 12시 00분";
 export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
 
-export const GROOM_NAME = "○○○";
+export const GROOM_NAME = "김학균";
 export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const GROOM_FATHER_NAME = "○○○";
 export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const GROOM_MOTHER_NAME = "○○○";
 export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 
-export const BRIDE_NAME = "○○○";
+export const BRIDE_NAME = "황이예진";
 export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const BRIDE_FATHER_NAME = "○○○";
 export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
